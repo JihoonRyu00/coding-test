9988edbdbb381fde92d51c593caf97fbbcacc321
JihoonRyu00 <harryryujh@naver.com>
2026-01-24 15:59:28 +0900

    [BOJ] 2739

---
+++ class/1/2739.py
@@ -0,0 +1,9 @@
+# 2739.py
+# Hello World
+# https://www.acmicpc.net/problem/2739
+
+N=int(input())
+# for i in range(9):
+#     print(str(N)+' * '+str(i+1)+' = '+str(N*(i+1)))
+for i in range(1,10):
+    print(f"{N} * {i} = {N*i}")
