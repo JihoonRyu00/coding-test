9b4de9364e98d9d99e219a906827029760fce1df
JihoonRyu00 <harryryujh@naver.com>
2026-01-28 17:11:53 +0900

    [BOJ] 10952

---
+++ class/1/10952.py
@@ -0,0 +1,12 @@
+# 10952.py
+# A+B - 5
+# https://www.acmicpc.net/problem/10952
+
+import sys
+input=sys.stdin.readline
+
+while True:
+    A,B=map(int,input().split())
+    if A==0 and B==0:
+        break
+    print(A+B)
